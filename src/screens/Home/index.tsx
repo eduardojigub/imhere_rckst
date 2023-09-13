844c83ea522ef78a2785b4a856e3fbdcd9fadbfc
Eduardo Sousa <eduardobrunoit@gmail.com>
2023-09-12 22:04:20 -0400

    feat: create add button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
+  function handleAddParticipant() {
+    console.log('Adicionando participante');
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
       padding: 24,
     },
     eventName: {
-      color: '#fdfcfe',
+      color: '#ffff',
       fontSize: 24,
       fontWeight: 'bold',
       marginTop: 48,
@@ -16,6 +16,29 @@ export const styles = StyleSheet.create({
       color: '#6b6b6b',
       fontSize: 16,
       marginTop: 8,
+    },
+    input: {
+      height: 56,
+      backgroundColor: '#1f1e25',
+      borderRadius: 5,
+      color: '#ffff',
+      padding: 16,
+      fontSize: 16,
+    },
+    buttonText: {
+      color: '#ffff',
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    button: {
+      backgroundColor: '#a370f7',
+      marginTop: 16,
+      height: 56,
+      width: 56,
+      borderRadius: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+
     },
   });
   
